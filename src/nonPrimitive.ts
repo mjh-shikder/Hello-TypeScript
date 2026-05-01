9d4da671cd6adb0054f21990c5239428064a6a3d
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-05-01 22:02:56 +0600

    ts array and Tuple

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -13,4 +13,24 @@ mixedArray.push(2222, 'apple', false)
 console.log(mixedArray);
 
 
+// * TypeScript - Tuple
 
+let coordinates: [number, number] = [20, 30]
+
+// coordinates = [23, 43, 44] now, this will give an error
+
+console.log(coordinates);
+
+let cople: [string, string] = ['mjh', 'ant']
+
+cople = ['jubair', 'ant']
+
+let nameAndRoll : [string, number] = ['josim', 69]
+
+nameAndRoll = ['josh', 96]
+
+nameAndRoll[0] = 'rohim'
+
+let destination: [string, string, number] = ['dhaka', 'benapole', 3]
+
+destination = ['joshor', 'dhaka', 5]
