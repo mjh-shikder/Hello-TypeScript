9c1da93b74f1b6033b1f6d3eec1e12d5d63128cb
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-05-01 21:51:11 +0600

    common non premitive

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,16 @@
+//* Common Non Premitive Array, Object
+
+let bazarList : string[] = ['eggs', 'milk', 'sugar', 'salt', 'tomato']
+
+bazarList.push('beef')
+
+console.log(bazarList);
+
+let mixedArray: (string | number | boolean)[] = ['eggs', 1, 'milk', 4, 'sugar', 3, false, true]
+
+mixedArray.push(2222, 'apple', false)
+
+console.log(mixedArray);
+
+
+
