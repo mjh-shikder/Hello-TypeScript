e519ef518249412f56143f9864dda891342857bf
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-05-01 21:37:18 +0600

    premitive data in ts

---
+++ src/premitive.ts
@@ -0,0 +1,19 @@
+//* JS: string, number, boolean, undefined, null
+
+//* TS: never, unknown, void
+
+let userName: string = 'jubair';
+
+let userId = 423
+
+userName = "mjh"
+
+let isAdmin = false;
+isAdmin = true
+
+let x = undefined
+
+x = 5
+x= "five"
+
+console.log(x);
